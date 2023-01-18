b410812f0a0e2e6d577b27bef6d5a67b25bc6eb3
Angie Helen Rodas Sánchez <angiehelensanchez@gmail.com>
2023-01-18 12:40:00 +0100

    18.01.2

---
+++ Primeros pasos/OperacionesNumericas/Suma.cs
@@ -0,0 +1,19 @@
+//Suma de dos valores numéricos
+int firstNumber = 12;
+int secondNumber = 7;
+Console.WriteLine(firstNumber + secondNumber);
+
+//String + int
+string firstName = "Bob";
+int widgetsSold = 7;
+Console.WriteLine(firstName + " sold " + widgetsSold + " widgets.");
+
+//1.1
+string firstName = "Bob";
+int widgetsSold = 7;
+Console.WriteLine(firstName + " sold " + widgetsSold + 7 + " widgets.");
+//1.2
+string firstName = "Bob";
+int widgetsSold = 7;
+Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets.");
+
