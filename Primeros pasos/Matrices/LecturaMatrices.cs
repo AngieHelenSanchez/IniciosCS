4f205359e80256c426f61a608c8e49c64f068dd3
Angie Helen Rodas Sánchez <angiehelensanchez@gmail.com>
2023-01-19 19:23:06 +0100

    19.01.4

---
+++ Primeros pasos/Matrices/LecturaMatrices.cs
@@ -0,0 +1,19 @@
+/*
+string[] fraudulentOrderIDs = new string[3];
+
+fraudulentOrderIDs[0] = "A123";
+fraudulentOrderIDs[1] = "B456";
+fraudulentOrderIDs[2] = "C789";
+// fraudulentOrderIDs[3] = "D000";
+*/
+
+int[] inventory = { 200, 450, 700, 175, 250 };
+int sum = 0;
+int bin = 0;
+foreach (int items in inventory)
+{
+    sum += items;
+    bin++;
+    Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+}
+Console.WriteLine($"We have {sum} items in inventory.");
